7f7c63bb06d6563de5e5f3620c4b59a1533ec7a9
janschab <56731968+janschab@users.noreply.github.com>
2020-01-17 10:53:32 +0100

    Update IdsArray.js

--- js/IdsArray.js
+++ js/IdsArray.js
@@ -11,16 +11,13 @@ IdsArray.prototype.next = function() {
 IdsArray.prototype.isNext = function() {
     return this.currentIndex < this.length - 1;
 };
-IdsArray.prototype.isPrev = function() {
-    return this.currentIndex > 0;
-};
 IdsArray.prototype.prev = function() {
     if (this.currentIndex > 0) {
         this.currentIndex--;
     }
     return this;
 };
-IdsArray.prototype.currentIndex = 0;
-IdsArray.prototype.getCurrent = function() {
-    return this[this.currentIndex];
+IdsArray.prototype.isPrev = function() {
+    return this.currentIndex > 0;
 };
+IdsArray.prototype.currentIndex = 0;
